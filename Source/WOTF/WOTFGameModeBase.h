e565b9eb7687432dfcec9ed80ae42dfd69306221
Ram Tripathi <ramt57@gmail.com>
2023-06-03 13:24:30 +0530

    first commit

---
+++ Source/WOTF/WOTFGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WOTFGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WOTF_API AWOTFGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
